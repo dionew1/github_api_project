163720e898d455907766102018754b650bdfa959
Dione Wilson <dionew1@gmail.com>
2017-12-06 15:36:39 -0700

    Set-up gems for VCR, webmock, and basscss

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,6 @@ class DashboardController < ApplicationController
   def show
     response = Faraday.get("https://api.github.com/users/#{current_user.login}")
     user_info = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
   end
 
 end
