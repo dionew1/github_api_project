76d337ea5f2f6c43cf73ad6bfb4cbe4db5be763e
Dione Wilson <dionew1@gmail.com>
2017-12-06 15:36:39 -0700

    Set-up gems for VCR, webmock, and basscss

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'omniauth-github'
 gem 'figaro'
 gem 'faraday'
-
+gem 'basscss-rails'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,13 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<GITHUB_CLIENT_ID>') { ENV['GITHUB_CLIENT_ID'] }
+  config.filter_sensitive_data('<GITHUB_CLIENT_SECRET>') { ENV['GITHUB_CLIENT_SECRET'] }
+  config.filter_sensitive_data('<ACCESS_TOKEN>') { user.token }
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
