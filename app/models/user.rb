906a62d5633633e0f6abbd868ed2c23da111a600
Dione Wilson <dionew1@gmail.com>
2017-12-05 17:46:21 -0700

    finish oauth setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   def self.from_omniauth(auth_info)
-    user = self.find_or_create_by(uid: auth_info[:uid]) do |user|
-      user.uid      = auth_info.uid
-      user.login    = auth_info.extra.raw_info.login
-      user.token    = auth_info.credentials.token
+    user = self.find_or_create_by(uid: auth_info[:uid]) do |new_user|
+      new_user.uid      = auth_info.uid
+      new_user.login    = auth_info.extra.raw_info.login
+      new_user.token    = auth_info.credentials.token
     end
     verify_current_token(user, auth_info)
   end
